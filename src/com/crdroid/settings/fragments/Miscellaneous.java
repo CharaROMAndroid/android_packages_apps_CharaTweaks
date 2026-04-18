ef15c5f2c8e35fe4c2adfe2ca1f4c02480300632
Chara <bionichuman07@gmail.com>
2026-04-18 14:44:28 -0400

    Revert "Add missing import (Re-add VBMeta Fix)"
    This reverts commit de0cf7173b1531fd0d10ae6c64522828d81c5135.

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -22,7 +22,6 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.View;
