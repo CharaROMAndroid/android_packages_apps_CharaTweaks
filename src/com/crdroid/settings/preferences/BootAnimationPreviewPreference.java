Evolution-X/packages_apps_Evolver

Evolver: Introduce boot animation styles [3/3]
Co-authored-by: Alvin Francis <nivlafx@gmail.com>
Co-authored-by: Joey Huab <jhuab@edukasyon.ph>
Co-authored-by: AnierinB <anierin@evolution-x.org>
Co-authored-by: 0xsharkboy <achille@0xsharkboy.dev>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

src/org/evolution/settings/preferences/BootAnimationPreviewPreference.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evolution.settings.preferences;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.settings.R;
+
+import org.evolution.settings.utils.BootAnimationUtils;
+
+public class BootAnimationPreviewPreference extends Preference {
+
+    private ImageView mImageView;
+    private ProgressBar mLoadingSpinner;
+    private LoadPreviewTask mCurrentTask;
+
+    public BootAnimationPreviewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_boot_animation_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mImageView = (ImageView) holder.findViewById(R.id.boot_animation_preview_image);
+        mLoadingSpinner = (ProgressBar) holder.findViewById(R.id.boot_animation_loading_spinner);
+        loadBootAnimationPreview();
+    }
+
+    public void loadBootAnimationPreview() {
+        if (mCurrentTask != null && mCurrentTask.getStatus() != AsyncTask.Status.FINISHED) {
+            mCurrentTask.cancel(true);
+        }
+        int bootAnimStyle = BootAnimationUtils.getBootAnimStyle();
+        if (bootAnimStyle == 5 || bootAnimStyle == 6) {
+            if (mImageView != null) {
+                Drawable drawable = getContext().getDrawable(
+                        bootAnimStyle == 5 ? R.drawable.google_gemini : R.drawable.google_monet);
+                mImageView.setImageDrawable(drawable);
+            }
+        } else {
+            mCurrentTask = new LoadPreviewTask();
+            mCurrentTask.execute();
+        }
+    }
+
+    private class LoadPreviewTask extends AsyncTask<Void, Void, AnimationDrawable> {
+        @Override
+        protected AnimationDrawable doInBackground(Void... voids) {
+            if (isCancelled()) return null;
+            AnimationDrawable originalDrawable = BootAnimationUtils.getBootAnimationFrames(getContext());
+            if (originalDrawable == null) {
+                return null;
+            }
+            AnimationDrawable fixedDrawable = new AnimationDrawable();
+            for (int i = 0; i < originalDrawable.getNumberOfFrames(); i++) {
+                if (isCancelled()) return null;
+                Drawable frame = originalDrawable.getFrame(i);
+                int duration = originalDrawable.getDuration(i);
+                if (duration < 16) { // 16 ms is around 60fps
+                    duration = 1000 / 60; // Set to 60fps as a fallback
+                }
+                fixedDrawable.addFrame(frame, duration);
+            }
+            fixedDrawable.setOneShot(false); // Ensure the animation loops
+            return fixedDrawable;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mImageView != null) {
+                mImageView.setVisibility(View.GONE);
+            }
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(AnimationDrawable animationDrawable) {
+            if (isCancelled()) return;
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.GONE);
+            }
+            if (mImageView != null) {
+                mImageView.setVisibility(View.VISIBLE);
+            }
+            if (animationDrawable != null) {
+                mImageView.setImageDrawable(animationDrawable);
+                animationDrawable.start();
+            }
+        }
+    }
+}

src/org/evolution/settings/utils/BootAnimationUtils.java
@@ -1,0 +1,197 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evolution.settings.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.regex.Pattern;
+
+public class BootAnimationUtils {
+
+    private static final String TAG = "BootAnimationUtils";
+    private static final int DEFAULT_FRAME_DURATION = 1000 / 30;
+
+    private static final String[] BOOT_ANIMATION_FILES = {
+        "/product/media/bootanimation.zip",
+        "/product/media/bootanimation_evo_anniv.zip",
+        "/product/media/bootanimation_cm.zip",
+        "/product/media/bootanimation_ctos.zip",
+        "/product/media/bootanimation_cyberpunk.zip",
+        "/product/media/bootanimation_google.zip",
+        "/product/media/bootanimation_google_monet.zip",
+        "/product/media/bootanimation_rr.zip",
+        "/product/media/bootanimation_valorant.zip",
+        "/data/misc/bootanim/bootanimation.zip",
+    };
+
+    public static AnimationDrawable getBootAnimationFrames(Context context) {
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        String selectedBootAnimation = getSelectedBootAnimation();
+        if (selectedBootAnimation != null) {
+            File bootAnimationFile = new File(selectedBootAnimation);
+            if (bootAnimationFile.exists()) {
+                try (ZipFile zipFile = new ZipFile(bootAnimationFile)) {
+                    int frameDuration = getFrameDuration(zipFile);
+                    int partCount = getPartCount(zipFile);
+                    if (partCount == 0) {
+                        List<Rect> trimData = loadTrimData(zipFile, "part0");
+                        loadFramesFromPart(context, zipFile, animationDrawable, "part0", frameDuration, trimData);
+                    } else {
+                        for (int i = 0; i < partCount; i++) {
+                            String partName = "part" + i;
+                            List<Rect> trimData = loadTrimData(zipFile, partName);
+                            loadFramesFromPart(context, zipFile, animationDrawable, partName, frameDuration, trimData);
+                        }
+                    }
+                    animationDrawable.setOneShot(false);
+                } catch (Exception e) {
+                    Log.e(TAG, "Error loading boot animation frames", e);
+                }
+            }
+        }
+        return animationDrawable;
+    }
+    
+    public static int getBootAnimStyle() {
+        return SystemProperties.getInt("persist.sys.bootanimation_style", 0);
+    }
+
+    public static String getSelectedBootAnimation() {
+        final int style = getBootAnimStyle();
+        if (style >= 0 && style < BOOT_ANIMATION_FILES.length) {
+            return BOOT_ANIMATION_FILES[style];
+        }
+        return null;
+    }
+
+    private static int getFrameDuration(ZipFile zipFile) {
+        try {
+            ZipEntry descEntry = zipFile.getEntry("desc.txt");
+            if (descEntry != null) {
+                InputStream is = zipFile.getInputStream(descEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line = reader.readLine();
+                if (line != null) {
+                    String[] parts = line.split(" ");
+                    if (parts.length >= 3) {
+                        int fps = Integer.parseInt(parts[2]);
+                        return 1000 / fps;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading desc.txt", e);
+        }
+        return DEFAULT_FRAME_DURATION;
+    }
+
+    private static int getPartCount(ZipFile zipFile) {
+        int partCount = 0;
+        try {
+            ZipEntry descEntry = zipFile.getEntry("desc.txt");
+            if (descEntry != null) {
+                InputStream is = zipFile.getInputStream(descEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains("part")) {
+                        partCount++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading desc.txt", e);
+        }
+        return partCount - 1;
+    }
+
+    private static List<Rect> loadTrimData(ZipFile zipFile, String partName) {
+        List<Rect> trimRects = new ArrayList<>();
+        try {
+            ZipEntry trimEntry = zipFile.getEntry(partName + "/trim.txt");
+            if (trimEntry != null) {
+                InputStream is = zipFile.getInputStream(trimEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split("[x+]");
+                    if (parts.length == 4) {
+                        int width = Integer.parseInt(parts[0]);
+                        int height = Integer.parseInt(parts[1]);
+                        int x = Integer.parseInt(parts[2]);
+                        int y = Integer.parseInt(parts[3]);
+                        trimRects.add(new Rect(x, y, x + width, y + height));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading trim data", e);
+        }
+        return trimRects;
+    }
+
+    private static void loadFramesFromPart(Context context, ZipFile zipFile, 
+        AnimationDrawable animationDrawable, String partName, int frameDuration, List<Rect> trimData) {
+        try {
+            Pattern pngPattern = Pattern.compile(partName + "/.*\\.png$");
+            Pattern jpgPattern = Pattern.compile(partName + "/.*\\.jpg$");
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            int frameIndex = 0;
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                if (pngPattern.matcher(entryName).matches() || jpgPattern.matcher(entryName).matches()) {
+                    try (InputStream is = zipFile.getInputStream(entry)) {
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        if (frameIndex < trimData.size()) {
+                            Rect trimRect = trimData.get(frameIndex);
+                            int adjustedWidth = Math.min(trimRect.width(), bitmap.getWidth() - trimRect.left);
+                            int adjustedHeight = Math.min(trimRect.height(), bitmap.getHeight() - trimRect.top);
+                            if (adjustedWidth > 0 && adjustedHeight > 0) {
+                                bitmap = Bitmap.createBitmap(bitmap, trimRect.left, trimRect.top, adjustedWidth, adjustedHeight);
+                            } else {
+                                //Log.w(TAG, "Trim rectangle exceeds bitmap dimensions, skipping trim for frame " + frameIndex);
+                            }
+                        }
+                        Drawable frame = new BitmapDrawable(context.getResources(), bitmap);
+                        animationDrawable.addFrame(frame, frameDuration);
+                        frameIndex++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading frames from " + partName, e);
+        }
+    }
+}

CharaROMAndroid/android_packages_apps_CharaTweaks

src/com/crdroid/settings/preferences/BootAnimationPreviewPreference.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.preferences;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.settings.R;
+
+import com.crdroid.settings.utils.BootAnimationUtils;
+
+public class BootAnimationPreviewPreference extends Preference {
+
+    private ImageView mImageView;
+    private ProgressBar mLoadingSpinner;
+    private LoadPreviewTask mCurrentTask;
+
+    public BootAnimationPreviewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.boot_animation_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mImageView = (ImageView) holder.findViewById(R.id.boot_animation_preview_image);
+        mLoadingSpinner = (ProgressBar) holder.findViewById(R.id.boot_animation_loading_spinner);
+        loadBootAnimationPreview();
+    }
+
+    public void loadBootAnimationPreview() {
+        if (mCurrentTask != null && mCurrentTask.getStatus() != AsyncTask.Status.FINISHED) {
+            mCurrentTask.cancel(true);
+        }
+        int bootAnimStyle = BootAnimationUtils.getBootAnimStyle();
+        if (bootAnimStyle == 7 || bootAnimStyle == 8) {
+            if (mImageView != null) {
+                Drawable drawable = getContext().getDrawable(
+                        bootAnimStyle == 7 ? R.drawable.google_gemini : R.drawable.google_monet);
+                mImageView.setImageDrawable(drawable);
+            }
+        } else {
+            mCurrentTask = new LoadPreviewTask();
+            mCurrentTask.execute();
+        }
+    }
+
+    private class LoadPreviewTask extends AsyncTask<Void, Void, AnimationDrawable> {
+        @Override
+        protected AnimationDrawable doInBackground(Void... voids) {
+            if (isCancelled()) return null;
+            AnimationDrawable originalDrawable = BootAnimationUtils.getBootAnimationFrames(getContext());
+            if (originalDrawable == null) {
+                return null;
+            }
+            AnimationDrawable fixedDrawable = new AnimationDrawable();
+            for (int i = 0; i < originalDrawable.getNumberOfFrames(); i++) {
+                if (isCancelled()) return null;
+                Drawable frame = originalDrawable.getFrame(i);
+                int duration = originalDrawable.getDuration(i);
+                if (duration < 16) { // 16 ms is around 60fps
+                    duration = 1000 / 60; // Set to 60fps as a fallback
+                }
+                fixedDrawable.addFrame(frame, duration);
+            }
+            fixedDrawable.setOneShot(false); // Ensure the animation loops
+            return fixedDrawable;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mImageView != null) {
+                mImageView.setVisibility(View.GONE);
+            }
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(AnimationDrawable animationDrawable) {
+            if (isCancelled()) return;
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.GONE);
+            }
+            if (mImageView != null) {
+                mImageView.setVisibility(View.VISIBLE);
+            }
+            if (animationDrawable != null) {
+                mImageView.setImageDrawable(animationDrawable);
+                animationDrawable.start();
+            }
+        }
+    }
+}

src/com/crdroid/settings/utils/BootAnimationUtils.java
@@ -1,0 +1,201 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.regex.Pattern;
+
+public class BootAnimationUtils {
+
+    private static final String TAG = "BootAnimationUtils";
+    private static final int DEFAULT_FRAME_DURATION = 1000 / 30;
+
+    private static final String[] BOOT_ANIMATION_FILES = {
+        "/product/media/bootanimation_evo_default.zip",
+        "/product/media/bootanimation_evo_reveal.zip",
+        "/product/media/bootanimation_aokp.zip",
+        "/product/media/bootanimation_cm.zip",
+        "/product/media/bootanimation_ctos.zip",
+        "/product/media/bootanimation_cyberpunk.zip",
+        "/product/media/bootanimation_du.zip",
+        "/product/media/bootanimation_google.zip",
+        "/product/media/bootanimation_google_monet.zip",
+        "/product/media/bootanimation_pac.zip",
+        "/product/media/bootanimation_rr.zip",
+        "/product/media/bootanimation_slim.zip",
+        "/product/media/bootanimation_valorant.zip",
+        "/data/misc/bootanim/bootanimation.zip",
+    };
+
+    public static AnimationDrawable getBootAnimationFrames(Context context) {
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        String selectedBootAnimation = getSelectedBootAnimation();
+        if (selectedBootAnimation != null) {
+            File bootAnimationFile = new File(selectedBootAnimation);
+            if (bootAnimationFile.exists()) {
+                try (ZipFile zipFile = new ZipFile(bootAnimationFile)) {
+                    int frameDuration = getFrameDuration(zipFile);
+                    int partCount = getPartCount(zipFile);
+                    if (partCount == 0) {
+                        List<Rect> trimData = loadTrimData(zipFile, "part0");
+                        loadFramesFromPart(context, zipFile, animationDrawable, "part0", frameDuration, trimData);
+                    } else {
+                        for (int i = 0; i < partCount; i++) {
+                            String partName = "part" + i;
+                            List<Rect> trimData = loadTrimData(zipFile, partName);
+                            loadFramesFromPart(context, zipFile, animationDrawable, partName, frameDuration, trimData);
+                        }
+                    }
+                    animationDrawable.setOneShot(false);
+                } catch (Exception e) {
+                    Log.e(TAG, "Error loading boot animation frames", e);
+                }
+            }
+        }
+        return animationDrawable;
+    }
+
+    public static int getBootAnimStyle() {
+        return SystemProperties.getInt("persist.sys.bootanimation_style", 0);
+    }
+
+    public static String getSelectedBootAnimation() {
+        final int style = getBootAnimStyle();
+        if (style >= 0 && style < BOOT_ANIMATION_FILES.length) {
+            return BOOT_ANIMATION_FILES[style];
+        }
+        return null;
+    }
+
+    private static int getFrameDuration(ZipFile zipFile) {
+        try {
+            ZipEntry descEntry = zipFile.getEntry("desc.txt");
+            if (descEntry != null) {
+                InputStream is = zipFile.getInputStream(descEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line = reader.readLine();
+                if (line != null) {
+                    String[] parts = line.split(" ");
+                    if (parts.length >= 3) {
+                        int fps = Integer.parseInt(parts[2]);
+                        return 1000 / fps;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading desc.txt", e);
+        }
+        return DEFAULT_FRAME_DURATION;
+    }
+
+    private static int getPartCount(ZipFile zipFile) {
+        int partCount = 0;
+        try {
+            ZipEntry descEntry = zipFile.getEntry("desc.txt");
+            if (descEntry != null) {
+                InputStream is = zipFile.getInputStream(descEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains("part")) {
+                        partCount++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading desc.txt", e);
+        }
+        return partCount - 1;
+    }
+
+    private static List<Rect> loadTrimData(ZipFile zipFile, String partName) {
+        List<Rect> trimRects = new ArrayList<>();
+        try {
+            ZipEntry trimEntry = zipFile.getEntry(partName + "/trim.txt");
+            if (trimEntry != null) {
+                InputStream is = zipFile.getInputStream(trimEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split("[x+]");
+                    if (parts.length == 4) {
+                        int width = Integer.parseInt(parts[0]);
+                        int height = Integer.parseInt(parts[1]);
+                        int x = Integer.parseInt(parts[2]);
+                        int y = Integer.parseInt(parts[3]);
+                        trimRects.add(new Rect(x, y, x + width, y + height));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading trim data", e);
+        }
+        return trimRects;
+    }
+
+    private static void loadFramesFromPart(Context context, ZipFile zipFile, 
+        AnimationDrawable animationDrawable, String partName, int frameDuration, List<Rect> trimData) {
+        try {
+            Pattern pngPattern = Pattern.compile(partName + "/.*\\.png$");
+            Pattern jpgPattern = Pattern.compile(partName + "/.*\\.jpg$");
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            int frameIndex = 0;
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                if (pngPattern.matcher(entryName).matches() || jpgPattern.matcher(entryName).matches()) {
+                    try (InputStream is = zipFile.getInputStream(entry)) {
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        if (frameIndex < trimData.size()) {
+                            Rect trimRect = trimData.get(frameIndex);
+                            int adjustedWidth = Math.min(trimRect.width(), bitmap.getWidth() - trimRect.left);
+                            int adjustedHeight = Math.min(trimRect.height(), bitmap.getHeight() - trimRect.top);
+                            if (adjustedWidth > 0 && adjustedHeight > 0) {
+                                bitmap = Bitmap.createBitmap(bitmap, trimRect.left, trimRect.top, adjustedWidth, adjustedHeight);
+                            } else {
+                                //Log.w(TAG, "Trim rectangle exceeds bitmap dimensions, skipping trim for frame " + frameIndex);
+                            }
+                        }
+                        Drawable frame = new BitmapDrawable(context.getResources(), bitmap);
+                        animationDrawable.addFrame(frame, frameDuration);
+                        frameIndex++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading frames from " + partName, e);
+        }
+    }
+}