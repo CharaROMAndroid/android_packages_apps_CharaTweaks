0dce7f7e905fb0ba7120c570d95627532d97acca
Chara <bionichuman07@gmail.com>
2026-07-05 19:15:41 -0400

    Fix compile errors, deprecate some boot animations

--- src/com/crdroid/settings/utils/BootAnimationUtils.java
+++ src/com/crdroid/settings/utils/BootAnimationUtils.java
@@ -42,24 +42,15 @@ public class BootAnimationUtils {
     private static final String TAG = "BootAnimationUtils";
     private static final int DEFAULT_FRAME_DURATION = 1000 / 30;
 
-    // Public style index constants — keep in sync with BOOT_ANIMATION_FILES order
-    public static final int STYLE_DEFAULT            = 0;
-    public static final int STYLE_GOOGLE             = 7;
-    public static final int STYLE_GOOGLE_MONET       = 8;
-    public static final int STYLE_CUSTOM             = 13;
 
     private static final String[] BOOT_ANIMATION_FILES = {
         "/product/media/bootanimation.zip", //0
         "/product/media/bootanimation_evo_default.zip", //1
         "/product/media/bootanimation_evo_reveal.zip", //2
         "/product/media/bootanimation_aokp.zip", //3
-        "/product/media/bootanimation_cr.zip", //4
         "/product/media/bootanimation_cm.zip", //5
         "/product/media/bootanimation_ctos.zip", //6
-        "/product/media/bootanimation_atvr.zip", //7
         "/product/media/bootanimation_du.zip", //8
-        "/product/media/bootanimation_google.zip", //9
-        "/product/media/bootanimation_google_monet.zip", //10
         "/product/media/bootanimation_pac.zip", //11
         "/product/media/bootanimation_rr.zip", //12
         "/product/media/bootanimation_slim.zip", //13
